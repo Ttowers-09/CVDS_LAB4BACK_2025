2058b007531f45c7a8ee1821fa47bbdad18d4bf5
David Salomon Baena Rubio <dsbaenar@gmail.com>
2025-02-13 15:14:30 -0500

    Creacion del proyecto backend con mvn

---
+++ app-core/src/main/java/com/laboratoryreservations/app_core/AppCoreApplication.java
@@ -0,0 +1,13 @@
+package com.laboratoryreservations.app_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppCoreApplication.class, args);
+	}
+
+}
