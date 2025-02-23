a4d9967fd5d2c1cce6e86a0d2df180aaa684821e
camiloaqrz14@gmail.com <camiloaqrz14@gmail.com>
2025-02-22 23:09:31 -0500

    Conexion mongobd

---
+++ app_core/src/main/java/com/cvds/eci/laboratoryreservations/app_core/AppCoreApplication.java
@@ -0,0 +1,13 @@
+package com.cvds.eci.laboratoryreservations.app_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppCoreApplication.class, args);
+	}
+
+}
