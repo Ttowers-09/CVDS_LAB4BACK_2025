b8b9cf02807e39a341a582213eb5bc4c52c54bbe
David Salomon Baena Rubio <dsbaenar@gmail.com>
2025-04-02 18:50:54 -0500

    config seguridad

--- src/main/java/com/cvds/eci/laboratoryreservations/app_core/config/SecurityConfig.java
+++ src/main/java/com/cvds/eci/laboratoryreservations/app_core/config/SecurityConfig.java
@@ -87,7 +87,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("https://mango-tree-0de7e7d0f.6.azurestaticapps.net"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
